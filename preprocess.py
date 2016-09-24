5ecea740700f923237352d8918f4078e53c9b1c6
Aristide <yedtoss@Aristides-MacBook-Pro.local>
2016-09-24 20:57:52 +0200

    Added multi-armed bandits support

--- preprocess.py
+++ preprocess.py
@@ -7,8 +7,6 @@ import settings
 from workalendar.europe import France
 
 
-
-
 def read_product(filename=None):
     """
     This function reads the product database
@@ -71,25 +69,35 @@ def read_temp(filename=None):
 def get_weather_on_date(date, meteo_day, store_id):
     """
     This function gives us all the weather information/prediction for a given day in a given store
+    :param date the date we want to query
+    :param meteo_day data containing all meteo
+    :param store_id the store where the meteo
+    :return the weather data on that date at that store
     """
     return meteo_day[(meteo_day['STO_EAN'] == store_id) & (meteo_day['DATE_KEY'] == date)]
 
 
 def get_volume_product_on_date(product_barcode, date, store_id, transactions):
     """
-    This function gives us the sum of all sales value for a given product on a given date
+    This function gives us the sum of all sales value for a given product on a given date.
+    It also return the quantity of products sold in weight
+    :param product_barcode the barcode of the product
+    :param the date of the transaction (Only year, month, day)
+    :param store_id the store where the transactions were made
+    :param transactions the data containing all transactions made
+    :return the total sales and weight of the given product
     """
     transactions_day = transactions[(transactions['STO_EAN'] == store_id) &
                         (transactions['BARCODE'] == product_barcode) &
                         (transactions['TRX_DATETIME'] >= pd.to_datetime(date).date())
-                        & (transactions['TRX_DATETIME'] < (pd.to_datetime(date) + pd.DateOffset(1)))]
+                        &(transactions['TRX_DATETIME'] < (pd.to_datetime(date) + pd.DateOffset(1)))]
 
+    # If the transaction does not exist return None
     if transactions_day.empty:
         return None
 
     return {"price": np.sum(transactions_day['SAL_AMT_WTAX'].values),
             "weight": np.sum(transactions_day['SAL_UNIT_QTY_WEIGHT'].values)}
-
 
 
 def generate_day_type(date):
@@ -115,6 +123,8 @@ def generate_day_type(date):
 
 def generate_weather_conditions(temperature, temp_type):
     """
+    This function generates the weather conditions using a mini-type trapezium distribution
+    the temperature and its type
     """
 
     if temp_type == "MIN" or temperature < 5:
@@ -143,9 +153,17 @@ def generate_weather_conditions(temperature, temp_type):
             return 0
 
 
-def generate_training_testing_dataset(store_id, transactions, meteo_day, max_days=2500):
+def generate_training_testing_dataset(store_id, transactions, meteo_day, max_days=2500,
+                                      single_barcode=0):
     """
     This function generates the training and testing data
+    :param store_id the id of the store
+    :param transactions the data containing all transactions
+    :param meteo_day data containing the meted data
+    :param max_days the max number of days to use in the training
+    :param single_barcode is not None, indicates to only return training for a single
+     product whose position depends on single_barcode. If None all products are included
+     :return the data with features extracted
     """
 
     # Get the minimum and maximum of date in the transactions
@@ -158,52 +176,66 @@ def generate_training_testing_dataset(store_id, transactions, meteo_day, max_day
     # Get the list of unique products barcode in the transactions
     products_barcode = transactions['BARCODE'].unique()
 
+    # Only do one single barcode if activated
+    if single_barcode is not None:
+        products_barcode = [products_barcode[single_barcode]]
 
+
+    # Array to contain all training data
     all_data_first_level = []
+
     # For each day and for each product
     for day in xrange(num_days):
 
         print(day)
 
+        # If we have already considered more days than allowed, stop
         if day > max_days:
             break
 
+
+        # Get the date corresponding to this day
         date = min_date + pd.DateOffset(day)
-
-
-
         # Get the weather of the date
         weather = get_weather_on_date(date, meteo_day, store_id).head(n=1)
 
-        # If the weather is empty we skip this
+        # If the weather is empty we skip this day
         if weather.empty:
             continue
+
+        # For each product to include
         for product_barcode in products_barcode:
 
-            # Get the volume
+            # Get the volume and inventory data
             volume = get_volume_product_on_date(product_barcode, date, store_id, transactions)
 
+            # If no volume could be found skip this date,product pair
             if volume is None:
-                break
+                continue
 
+            # Get the type of the current date
             day_type = generate_day_type(date)
-
 
 
             # Generating complex features based on the simpler one
 
+            # This contains respectively yesterday, the day before yesterday and the same day as current one in
+            # previous week
             yesterday = date - pd.DateOffset(1)
             two_days_ago = date - pd.DateOffset(2)
             one_week_ago = date - pd.DateOffset(7)
 
+            # Get the day type of yesterday and 2 days ago
             day_type_yesterday = generate_day_type(yesterday)
             day_type_2days_ago = generate_day_type(two_days_ago)
-            #
+
+            # Get the volume of yesterday, 2days ago and 1 week ago
             volume_yesterday = get_volume_product_on_date(product_barcode, yesterday, store_id, transactions)
             volume_2days_ago = get_volume_product_on_date(product_barcode, two_days_ago, store_id, transactions)
             volume_one_week_ago = get_volume_product_on_date(product_barcode, one_week_ago, store_id, transactions)
 
 
+            # Get the total sales and the total weight of product done yesterday, 2 days ago and 1 week ago
             volume_price_yesterday = 0
             volume_weight_yesterday = 0
             if volume_yesterday is not None:
@@ -223,14 +255,8 @@ def generate_training_testing_dataset(store_id, transactions, meteo_day, max_day
                 volume_weight_one_week_ago = volume_one_week_ago["weight"]
 
 
-            #if volume_yesterday is not None:
-            #    volume_price_yesterday = volume_yesterday["price"]
-            #    volume_weight_yesterday = volume_yesterday["weight"]
 
-
-
-
-            # Using historic weather data
+            # Using historical weather data
             weather_yesterday = get_weather_on_date(yesterday, meteo_day, store_id).head(n=1)
             temperature_min_yesterday = 0
             temperature_max_yesterday = 0
@@ -243,6 +269,8 @@ def generate_training_testing_dataset(store_id, transactions, meteo_day, max_day
             #       weather['PRECIPITATION_VALUE'].values[0], weather['SUNSHINE_DURATION'].values[0],
             #       weather['SNOW_DEPTH'].values[0], day_type, volume["price"], volume["weight"]]
 
+
+            # Saving Features
             tmp = [weather['TEMPERATURE_VALUE_MIN'].values[0], weather['TEMPERATURE_VALUE_MAX'].values[0],
                    day_type, volume["price"], volume_price_yesterday,volume_weight_yesterday,
                    volume_price_2days_ago, volume_weight_2days_ago,
@@ -252,11 +280,7 @@ def generate_training_testing_dataset(store_id, transactions, meteo_day, max_day
 
             all_data_first_level.append(tmp)
 
-            break
-
-
     return all_data_first_level
-
 
 
 def get_preprocess_data():
@@ -264,16 +288,13 @@ def get_preprocess_data():
     # Reading transaction
     trans = read_transactions()
 
+    # Eliminating row containing missing data
     trans = trans.dropna()
 
     # Reading meteo data
     md = read_meteo_day()
 
-
     return np.asarray(generate_training_testing_dataset(settings.DIJON_ID, trans, md), dtype=np.float)
-
-
-
 
 
 if __name__ == "__main__":
@@ -281,6 +302,7 @@ if __name__ == "__main__":
     Main module
     """
 
+    # These are just for quick testing of this module
     products = read_product()
 
     # trans.loc[1][1]
--- training.py
+++ training.py
@@ -2,46 +2,115 @@ __author__ = 'yedtoss'
 
 import xgboost as xgb
 import preprocess
-from sklearn.metrics import mean_absolute_error, mean_squared_error, accuracy_score
+from sklearn.metrics import mean_absolute_error
 from sklearn.cross_validation import train_test_split
 import numpy as np
-
-
+from sklearn.svm import SVR
 
 
 def compute_relative_absolute_error(y_true, y_pred):
+    """
+    This function compute the relative absolute error in percentage
+    :param y_true: the true demand
+    :param y_pred:  the forecasted prediction
+    :return: the relative absolute error
+    """
 
     return 100.*np.sum(abs(y_true-y_pred)/y_true)/len(y_true)
 
-def compute_relative_mean_error(y_true, y_pred):
-
-    return
 
 def train():
 
+    # Getting the data
     datas = preprocess.get_preprocess_data()
 
+    # Separating the features from the label
     X = datas[:, :-1]
     y = datas[:, datas.shape[1]-1]
 
-    print(X.shape, y.shape)
-
-    X = X.astype(np.float)
-    y = y.astype(np.float)
+    #X = X.astype(np.float)
+    #y = y.astype(np.float)
 
 
 
+    # Splitting the training set into test and validation set
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
+    # Training using the Gradient Boosting Tree algorithm
     gbm = xgb.XGBRegressor(max_depth=30, n_estimators=100, learning_rate=0.05).fit(X_train, y_train)
-
+    # Testing
     predictions = gbm.predict(X_test)
 
-    print(mean_absolute_error(y_test, predictions))
+    # Error for the Gradient Boosting Tree Algorithm
+    print('Mean Absolute Error of Gradient Boosting', mean_absolute_error(y_test, predictions))
+    print('Mean Relative Error of Gradient Boosting', compute_relative_absolute_error(y_test, predictions))
 
-    print(compute_relative_absolute_error(y_test, predictions))
+    # Plotting utility to see relations and importance of each features
+    #xgb.to_graphviz(gbm, num_trees=2)
 
-    #xgb.plot_importance(gbm)
+
+    # Training a deeper Gradient Boosting
+    gbm_deep = xgb.XGBRegressor(max_depth=50, n_estimators=200, learning_rate=0.05).fit(X_train, y_train)
+    # Testing
+    predictions_deep = gbm_deep.predict(X_test)
+    # Error for SVM Algorithm
+    print('Mean Absolute Error of Deeper Gradient Boosting', mean_absolute_error(y_test, predictions_deep))
+    print('Mean Relative Error of Deeper Gradient Boosting', compute_relative_absolute_error(y_test, predictions_deep))
+
+    print('The error of EXP4 is ', run_exp4([gbm, gbm_deep], X_test, y_test))
+
+
+def run_exp4(algorithms, X_test, y_test):
+    """
+    This function runs the multi-armed bandit EXP4 algorithm on some algorithms
+    and return the overall performance
+    :param algorithms: algorithms to learning in online
+    :param X_test: the test set
+    :param y_test: the true answer
+    :return: the mean relative absolute error
+    """
+
+    #Get the number of rounds
+    T = len(y_test)
+
+    # Get the number of arms
+    K = len(algorithms)
+
+    # Set the weights of each arms
+    W = np.ones(K)
+
+    # Set the error
+    error = 0.
+
+
+
+    p = (1./K)*np.ones(K)
+
+    # Set the exploration parameter gamma
+    gamma = min(1, np.sqrt(K*np.log(K)/((np.exp(1)-1)*T)))
+
+    for round in range(T):
+
+        # Get advice vectors
+        predictions = np.zeros(K)
+        for k in range(K):
+            predictions[k] = algorithms[k].predict(np.atleast_2d(X_test[round, :]))
+
+
+        # Set the probabilities of taking each arm
+        p = (1-gamma)*W*predictions/np.sum(W) + gamma/K
+
+        # Pick one arm and receives rewards
+        action = np.random.choice(range(K), p=p)
+        reward = 1-compute_relative_absolute_error([y_test[round]], predictions[action])
+        error += 1-reward
+
+        # Update the weights
+        W[action] = W[action]*np.exp(reward*(gamma/K)/p[action])
+
+    return error/T
+
+
 
 
 
